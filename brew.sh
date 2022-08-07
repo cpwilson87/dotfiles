e294c9e9147e0d14810d342167253082c32b7d4f
Chris Wilson <cpwilson87@outlook.com>
2022-08-07 08:15:19 +0100

    feat: add brew.sh

---
+++ brew.sh
@@ -0,0 +1,25 @@
+
+#############################################
+# Install Packages & Fonts
+#############################################
+
+# Browsers
+brew install firefox
+
+# CLI
+brew install gh
+brew install starship
+
+# Dev
+brew install node
+brew install alacritty
+brew install neovim
+
+# Utils
+brew install 1password
+brew install dropbox
+brew install rectangle
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew install font-hack
